c92e736eedf56a8a8fae7f5f6c9d5dbee39a2631
AliErcanOzgokce <aliercanozgokce@gmail.com>
2022-10-08 23:35:40 +0300

    fixing week 2 files

--- weekend-project-2/hardhat.config.ts
+++ weekend-project-2/hardhat.config.ts
@@ -8,7 +8,16 @@ const { REACT_APP_PROVIDER_RPC_KEY, REACT_APP_PRIVATE_KEY, REACT_APP_ETHERSCAN_K
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
-  paths: { tests: "test" },
+  paths: { tests: "tests" },
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${REACT_APP_PROVIDER_RPC_KEY}`,
+      accounts: [REACT_APP_PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: REACT_APP_ETHERSCAN_KEY,
+  },
 };
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
--- weekend-project-2/test/Test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-
-describe("when creating", async () => {
-    it("should create a new ballot", async () => {
-        const proposals = ["Cofee", "Tea"];
-        expect(proposals.length).to.equal(2); 
-    });
-});
