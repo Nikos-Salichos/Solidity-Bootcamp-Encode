692ddf5d9d30470d8f3d2fa155db98839a946399
AliErcanOzgokce <aliercanozgokce@gmail.com>
2022-10-08 23:31:20 +0300

    Changing token contract address to original address

--- weekend-project-3/scripts/DeployTokenizedBallot.ts
+++ weekend-project-3/scripts/DeployTokenizedBallot.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 namespace DeployTokenizedBallot {
   const proposals = ["Cofee", "Tea"];
   let bytes32Array: any[] = [];
-  const erc20Token = "0xb3CbfB95A57318A8764F0533F5e43600daf18B07";
+  const erc20Token = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const referenceBlock = 7734792;
 
   function convertStringArrayToBytes32() {
