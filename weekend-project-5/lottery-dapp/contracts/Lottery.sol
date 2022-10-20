90c2d97f03974e7235b81c333d62577e4cae3ac2
Nikos S <niksal1991@gmail.com>
2022-10-20 21:37:49 +0300

    Added Ownable

--- weekend-project-5/lottery-dapp/contracts/Lottery.sol
+++ weekend-project-5/lottery-dapp/contracts/Lottery.sol
@@ -2,39 +2,17 @@
 
 pragma solidity ^0.8.0;
 
-contract Lottery{
-    //manager is in charge of the contract 
-    address public manager;
-    //new player in the contract using array[] to unlimit number 
-    address[] public players;
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-    function lottery() public {
-        manager = msg.sender;
-    }
-    //to call the enter function we add them to players
-    function enter() public payable{
-        //each player is compelled to add a certain ETH to join
-        require(msg.value > .01 ether);
-        players.push(msg.sender);
-    }
-    //creates a random hash that will become our winner
-    function random() private view returns(uint){
-        return  uint (keccak256(abi.encode(block.timestamp,  players)));
-    }
-    function pickWinner() public restricted{
-        //only the manager can pickWinner
-        //require(msg.sender == manager);
-        //creates index that is gotten from func random % play.len
-        uint index = random() % players.length;
-        //pays the winner picked randomely(not fully random)
-        payable (players[index]).transfer(address(this).balance);
-        //empies the old lottery and starts new one
-        players = new address[](0);
-    }
+contract Lottery is Ownable{
+    uint public closingTime;
+    bool public betsOpen;
 
-    modifier restricted(){
-        require(msg.sender == manager);
-        _;
+    constructor(uint _betPrice, uint _betFree){}
 
-    }
+    function openBets(uint _closingTime) public onlyOwner{
+        require(_closingTime > block.timestamp,"Closing time must be in the future");
+        closingTime = _closingTime;
+    } 
+   
 }
