d00bb964c07ba57f72f79c53c2175d5103c36b4d
Nikos S <niksal1991@gmail.com>
2022-10-22 08:37:59 +0300

    Added input field for mintint to address

--- weekend-project-4/tokenized-ballot/src/assets/myTokenERC20Address.tsx
+++ weekend-project-4/tokenized-ballot/src/assets/myTokenERC20Address.tsx
@@ -1,1 +1,1 @@
-export const myTokenERC20Address = "0xb3CbfB95A57318A8764F0533F5e43600daf18B07";
+export const myTokenERC20Address = "0xb74a1d2E616E4bdCf0D61a281E9FC2A2000996eD";
--- weekend-project-4/tokenized-ballot/src/components/mint.tsx
+++ weekend-project-4/tokenized-ballot/src/components/mint.tsx
@@ -5,10 +5,20 @@ import { myTokenERC20 } from "../assets/myTokenERC20";
 
 function Mint({ accounts, setAccounts, amountOfTokens, setAmountOfTokens }: { accounts: any; setAccounts: any; amountOfTokens: any; setAmountOfTokens: any }) {
   const [transactionHash, setTransactionHash] = useState("");
+  const [mintERC20Tokens, setMintERC20Tokens] = useState("");
+  const [mintingToAccount, setMintingToAccount] = useState("");
+
+  const handleMintTokens = (event: { target: { value: any } }) => {
+    setMintERC20Tokens(event.target.value);
+  };
+
+  const handleMintingToAccount = (event: { target: { value: any } }) => {
+    setMintingToAccount(event.target.value);
+  };
 
   async function mintTokens() {
     try {
-      const claimList = ["0x05b4Ae8d9B4ccd9f6D470956968DD49274BdD16F", "0xe3a9a11232f4d52786ca61f56bb7fb01b00c80cd", "0xF0263339678ec2ad8C6722b5eCedDB904cBed19D"];
+      const claimList = ["0x05b4Ae8d9B4ccd9f6D470956968DD49274BdD16F", "0xe3a9a11232f4d52786ca61f56bb7fb01b00c80cd", "0xF0263339678ec2ad8C6722b5eCedDB904cBed19D", "0x3187Bf4b5B648F0E051EdC3f7ab54884aa37B262"];
 
       const upperCase = claimList.map((element) => {
         return element.toUpperCase();
@@ -16,23 +26,25 @@ function Mint({ accounts, setAccounts, amountOfTokens, setAmountOfTokens }: { ac
       let isAccountExists;
       if (accounts[0] !== "undefined") {
         isAccountExists = upperCase.includes(accounts[0].toUpperCase());
+        console.log(isAccountExists);
       }
 
       if (isAccountExists) {
-        const providerRpcKey = process.env.REACT_APP_PROVIDER_RPC_KEY;
-        const privateKey = process.env.REACT_APP_PRIVATE_KEY;
+        const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+        provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        console.log("Account:", signer.getAddress());
 
-        const provider = new ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${providerRpcKey}`);
-        const wallet = new ethers.Wallet(privateKey!, provider);
-        const signer = wallet.connect(provider);
+        // const providerRpcKey = process.env.REACT_APP_PROVIDER_RPC_KEY;
+        // const privateKey = process.env.REACT_APP_PRIVATE_KEY;
+        // const provider = new ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${providerRpcKey}`);
+        // const wallet = new ethers.Wallet(privateKey!, provider);
+        // const signer = wallet.connect(provider);
 
         const tokenContract = new ethers.Contract(myTokenERC20Address, myTokenERC20.abi, signer);
-        console.log(tokenContract);
-        const mintingTo = accounts[0];
 
         const mintingAmount = ethers.utils.parseEther("1");
-
-        const mintTx = await tokenContract.mint(mintingTo, mintingAmount);
+        const mintTx = await tokenContract.mint(mintingToAccount, mintingAmount);
 
         const receipt = await mintTx.wait();
 
@@ -62,9 +74,20 @@ function Mint({ accounts, setAccounts, amountOfTokens, setAmountOfTokens }: { ac
       <p>
         <b>Tokens minted:</b> {amountOfTokens}
       </p>
-      <button className="button" onClick={mintTokens}>
-        Mint
-      </button>
+      <p>
+        <b>Minting to account:</b> {mintingToAccount}
+      </p>
+      <div>
+        <button className="button" onClick={mintTokens}>
+          Mint
+        </button>
+      </div>
+      <div>
+        <input type="number" id="mint" name="mint" onChange={handleMintTokens} value={mintERC20Tokens} min="0" max="5" placeholder="Amount of ERC20 tokens to mint" />
+      </div>
+      <div>
+        <input type="text" id="mintingTo" name="mintingTo" onChange={handleMintingToAccount} value={mintingToAccount} placeholder="Minting to address" />
+      </div>
     </div>
   );
 }
