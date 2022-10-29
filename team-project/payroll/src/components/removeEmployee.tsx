d3f23969724da3834acc6c8db83780db6c039975
Nikos S <niksal1991@gmail.com>
2022-10-29 08:59:24 +0300

    Added removeEmployeeAsync

--- team-project/payroll/src/components/removeEmployee.tsx
+++ team-project/payroll/src/components/removeEmployee.tsx
@@ -11,6 +11,29 @@ function RemoveEmployee({ accounts, setAccounts }: { accounts: any; setAccounts:
     setEmployeeAddress(event.target.value);
   };
 
+  async function removeEmployeeAsync() {
+    if (accounts[0] !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+      provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      console.log("Account:", signer.getAddress());
+
+      const payroll = new ethers.Contract(payrollAddress, payrollContract.abi, signer);
+
+      const removeEmployee = await payroll.removeEmployee(employeeAddress);
+      const receipt = await removeEmployee.wait();
+      console.log(`receipt.transactionHash ${receipt.transactionHash}`);
+
+      setEmployeeAddress("");
+
+      if (receipt.transactionHash !== "undefined") {
+        setTransactionHash(receipt.transactionHash);
+      } else {
+        setTransactionHash("Transaction failed");
+      }
+    }
+  }
+
 
 }
 
