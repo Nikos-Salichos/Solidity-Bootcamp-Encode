f254783a3ac273dc8ea5b838734007b2e3cd5f67
Nikos S <niksal1991@gmail.com>
2022-10-30 08:40:53 +0200

    Fixed hardhat tests

--- team-project/payroll-tests/payrollConstructor/arguments.ts
+++ team-project/payroll-tests/payrollConstructor/arguments.ts
@@ -2,4 +2,4 @@ export const tokenName = "Team Project Group 8";
 export const tokenSymbol = "TPG8";
 export const initialCapital = 1000000000000000000n;
 
-module.exports = [tokenName, tokenSymbol, initialCapital];
+// module.exports = [tokenName, tokenSymbol, initialCapital];
