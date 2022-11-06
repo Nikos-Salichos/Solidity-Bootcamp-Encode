80ead2078d333c9f08445b3a63ddb6c15132999d
AliErcanOzgokce <aliercanozgokce@gmail.com>
2022-11-06 22:59:42 +0300

    Updates

--- team-project/payroll/src/assets/PayrollAddress.tsx
+++ team-project/payroll/src/assets/PayrollAddress.tsx
@@ -1,1 +1,1 @@
-export const payrollAddress = "0xC735408a81c8E8F8541B82ca6F903FBfAD932A9c";
+export const payrollAddress = "0x3B9BC66F2e0516c1d4295CAFa29aDe7349f2DCfc";
--- team-project/payroll/src/components/Pages/Admin Panel/employee-table.tsx
+++ team-project/payroll/src/components/Pages/Admin Panel/employee-table.tsx
@@ -4,6 +4,7 @@ import { payrollContract } from "../../../assets/PayrollContract";
 import { payrollAddress } from "../../../assets/PayrollAddress";
 import { Button, Icon, Table } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { formatEther } from "ethers/lib/utils";
 
 class EmployeeTable extends Component<any , any> {
     constructor(props: any) {
@@ -46,19 +47,23 @@ class EmployeeTable extends Component<any , any> {
 
     renderRow() {
         return this.state.employees.map((employee: any, index: any) => {
+            const unixTime = employee[3];
+            const date = new Date(unixTime*1000);
             return (
                 <Table.Row key={index}>
-                    <Table.Cell>{index}</Table.Cell>
-                    <Table.Cell>{employee[0].toString()}</Table.Cell>
-                    <Table.Cell>{employee[1].toString()}</Table.Cell>
-                    <Table.Cell>{employee[2].toString()}</Table.Cell>
-                    <Table.Cell>{employee[3].toString()}</Table.Cell>
-                    <Table.Cell>
+                    {employee[1] == 0 ? null : (<><Table.Cell>{index}</Table.Cell><Table.Cell>{employee[0].toString()}</Table.Cell><Table.Cell>{formatEther(employee[1].toString())} ETH</Table.Cell><Table.Cell>{unixTime == 0 ? 0 : (date.toUTCString())}</Table.Cell><Table.Cell>{employee[3].toString()}</Table.Cell><Table.Cell>
                         <Button floated='right' icon labelPosition='left' color="red" size='small' onClick={() => this.removeEmployee(employee[0].toString())}>
-                            <Icon name='edit' />
-                                Remove Employee
+                            <Icon name='trash' />
+                            Remove Employee
                         </Button>
-                    </Table.Cell>
+                    </Table.Cell><Table.Cell>
+                            <Link to="/update-salary">
+                                <Button floated='right' icon labelPosition='left' color="yellow" size='small'>
+                                    <Icon name='edit' />
+                                    Update Salary
+                                </Button>
+                            </Link>
+                        </Table.Cell></>)}
                 </Table.Row>
             );
         });
@@ -76,6 +81,7 @@ class EmployeeTable extends Component<any , any> {
                                     <Table.HeaderCell>Last Payment</Table.HeaderCell>
                                     <Table.HeaderCell>Payment Count</Table.HeaderCell>
                                     <Table.HeaderCell>Remove Employee</Table.HeaderCell>
+                                    <Table.HeaderCell>Update Salary</Table.HeaderCell>
                                 </Table.Row>
                                 </Table.Header>
                                 <Table.Body>
@@ -83,7 +89,7 @@ class EmployeeTable extends Component<any , any> {
                                 </Table.Body>
                                 <Table.Footer fullWidth>
                                 <Table.Row>
-                                    <Table.HeaderCell colSpan='6'>
+                                    <Table.HeaderCell colSpan='7'>
                                     <Link to="/add-employee">
                                         <Button
                                             floated='right'
