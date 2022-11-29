0abad6612aa75f7705ecf422a9191e3a65fdb924
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-11-29 07:33:10 +0200

    Update PayrollFactory.sol

--- team-project/payroll/contracts/PayrollFactory.sol
+++ team-project/payroll/contracts/PayrollFactory.sol
@@ -1,6 +1,10 @@
 contract PayrollFactory{ 
     Payroll[] public Payrolls;
 
-   
+    function createCryptoCoin(string memory tokenName, string memory tokenSymbol, uint initialTokenCapital, uint ratio) public {
+        Payroll cryptoCoin = new Payroll(tokenName,tokenSymbol,initialTokenCapital,ratio);
+        Payrolls.push(cryptoCoin);
+    }
+
 
 }
